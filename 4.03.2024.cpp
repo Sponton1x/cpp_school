ba9a8d2baf22f446686179bc8e3547f427479e5e
Connective <76740905+Sponton1x@users.noreply.github.com>
2024-03-10 21:36:55 +0100

    Update 4.03.2024.cpp

--- 4.03.2024.cpp
+++ 4.03.2024.cpp
@@ -13,6 +13,25 @@ int int_suma(int a, int b)
 	return a+b;
 }
 
+int int_roznica(int a, int b) {
+	return a-b;
+}
+
+int int_mnozenie(int a, int b) {
+	return a*b;
+}
+
+int int_dzielenie(int a, int b) {
+	if (b != O)
+	{
+		std::cout << "Podana liczba b jest 0. Nie można dzielić przez 0! Jedyneczka z matmy :)" << std::endl;
+		return 0;
+	}
+	else
+	{
+		return a/b;
+	}
+}
 
 int main() 
 {
@@ -34,7 +53,10 @@ int main()
 	void_suma(a, b);
 	
 	std::cout << "Suma " << int_suma(a, b) << std::endl;
-	
+	std::cout << "Różnica " << int_roznica(a, b) << std::endl;
+	std::cout << "Mnożenie " << int_mnozenie(a, b) << std::endl;
+	std::cout << "Dzielenie " << int_dzielenie(a, b) << std::endl;
+		
 	std::cout << "Czy chcesz kontynuowaæ (t/T - tak, inny znak - nie)? ";
     
 	} while (std::getchar() == 't' or std::getchar() == 'T');
