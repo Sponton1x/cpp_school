b2ce1058a12af7a49c95bc9617b1c28e16847cb5
Connective <76740905+Sponton1x@users.noreply.github.com>
2024-03-23 14:13:03 +0100

    Update algorytm.cpp

--- algorytm.cpp
+++ algorytm.cpp
@@ -15,27 +15,42 @@ std::string calc_2_char_number(int a) {
 	}
 }
 
-bool calc_3_char_number(int a) {
-	int b = a%100; // 55
-	int result_1 = a-b; // 500
-	int result_2 = result_1/100; // 5
-	int result_3 = result_1/10;
-	
-	std::cout << b << std::endl;
-	std::cout << result_1 << std::endl;
-	std::cout << result_3 << std::endl;
-	
+bool is_all_numer_same(int liczba) {
+    int cyfra = liczba % 10; // Pobieramy ostatnią cyfrę
+    liczba /= 10; // Usuwamy ostatnią cyfrę
+
+    while (liczba > 0) {
+        if (liczba % 10 != cyfra) // Jeśli cyfra się zmienia, to liczba nie ma wszystkich cyfr jednakowych
+            return false;
+        liczba /= 10; // Przechodzimy do kolejnej cyfry
+    }
+
+    return true; // Jeśli doszliśmy do tego miejsca, to wszystkie cyfry były takie same
 }
 
 int main() {
 	int a;
+	int liczba;
 	
-	std::cout << "Podaj liczbe ";
-	
-	std::cin >> a;
-	//std::cout << calc(a);
-	std::cout << calc_3_char_number(a);
-	
+	std::cout << "Podaj liczbe "; std::cin >> a;
+	if (a >= 100) {
+		std::cerr << "Podana liczba nie jest dwucyfrowa" << std::endl;
+	}
+	else {
+		std::cout << calc_3_char_number(a);
+	}
+
+	if (liczba < 100 || liczba > 999) {
+		std::cerr << "Podana liczba nie jest trzycyfrowa." << std::endl;
+	}
+	else {
+		if (is_all_numer_same(liczba)) {
+			std::cout << "Wprowadzona liczba ma wszystkie cyfry takie same." << std::endl;
+		}
+		else {
+			std::cout << "Wprowadzona liczba nie ma wszystkich cyfr takich samych." << std::endl;
+		}
+	}
 	
 	return 0;
 }
