a9e8686c500ab29c0cea03d97686b21fd12e8314
Connective <76740905+Sponton1x@users.noreply.github.com>
2024-04-22 09:55:11 +0200

    Update tablica1.cpp

--- tablica1.cpp
+++ tablica1.cpp
@@ -1,24 +1,39 @@
-#include <iostream>
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<conio.h>
 
 int main() {
-	int tab[5] = {3, 5, 6, 3, 7}; // jeœli podamy mniej ni¿ zak³adamy => zwróci 0 w puste komórki
-	int count;
-	int suma = 0;
+	int tab[15];
+	int nums;
+	int suma_p;
+	int suma_np;
 	
-	for(int i=0; i<=4; i++) { // jeœli i podamy wiêksze ni¿ zakres tablicy, to zwróci nam przypadkowe wartoœci z pamiêci RAM.
-		if (tab[i] % 2 == 0) {
-			count += 1;
+	
+	std::cout << "Podaj zakres liczb psudolosowych : 1-";
+	std::cin >> nums;
+	
+	do {
+		srand(time(0)); // inicjalizacja generatora liczb pseudolosowych
+		
+		for(int i=0; i<15; i++) {
+			tab[i] = (rand()%nums) + 1; // wywołanie generatora
+			std::cout << tab[i] << "\t"; 
 		}
-		suma += tab[i];	
-	}
-	if (count > 0 ) {
-		std::cout << "Jest " << count << " liczb parzystych" << std::endl;
-	}
-	else {
-		std::cout << "Nie ma liczb parzystych" << std::endl;
-	}
-	std::cout << "Suma tablicy wynosi " << suma << std::endl;
-	
+		
+		for(int i=0; i<15; i++){
+			if (tab[i] % 2 == 0) {
+				suma_p += tab[i];
+			}
+			else {
+				suma_np += tab[i];
+			}
+		}
+		std::cout << "Suma parzyste " << suma_p << std::endl;
+		std::cout << "Suma nieparzyste " << suma_np << std::endl;		
+		
+	std::cout << "Czy chcesz kontynuowac? t/n\n";
+	} while(getch()==116);
 	
 	return 0;
 }
